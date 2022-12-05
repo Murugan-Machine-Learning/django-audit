b3d6dff49a2ecbe210b8db76ad45ce72a4c595d4
muruganeauditor <murugan@eauditoroffice.com>
2022-12-05 14:51:55 +0530

    admin changes

--- api/admin.py
+++ api/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import UdyamForm
 # Register your models here.
 
-admin.site.register(UdyamForm)
+class UdyamFormAdmin(admin.ModelAdmin):
+    list_display =  ('name', 'email', 'mobile','pan','aadhaar')
+
+admin.site.register(UdyamForm, UdyamFormAdmin)
